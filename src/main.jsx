4ff12ac6018f3ec02684c3339aa9ace9236fc16e
TaherunNasaToma <taheruntoma@gmail.com>
2025-04-22 02:27:26 +0600

    added navbar

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {
-  createBrowserRouter,
+  
   RouterProvider,
 } from "react-router";
+import { router } from './Routes/Routes.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello World</div>,
-  },
-]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
